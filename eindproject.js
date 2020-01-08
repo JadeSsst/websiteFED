bd846a9573e42f457761987dd95444502ceda460
JadeSsst <jadetje@gmail.com>
2020-01-08 10:22:01 +0100

    Delete eindproject.js

--- eindproject.js
+++
@@ -1,9 +0,0 @@
-var button = document.querySelector("button");
-var element = document.querySelector("div");
-
-
-button.addEventListener("click", function() {
-    element.classList.toggle("verschijn");
-    button.style.display = "none";
-});
-

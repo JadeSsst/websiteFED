9f78729e8c2aa575a2178d35b1cbc879513ba0a1
JadeSsst <jadetje@gmail.com>
2020-01-18 17:03:08 +0100

    Add files via upload

--- js/eindproject.js
+++ js/eindproject.js
@@ -1,7 +1,9 @@
+//maakt 2 variable aan voor div uit div en button uit div
 var button = document.querySelector("div > button");
 var grapje = document.querySelector("div > div");
 
 
+// functie zodat wanneer er geklikt wordt, class 'verschijn' gekoppeld aan de div uit de div, var grapje, en verdwijnt de oude button, var button
 button.addEventListener("click", function() {
     grapje.classList.toggle("verschijn");
     button.style.display = "none";
